9ad17ca3e0ca8474ae18eb8580cbf260986ed446
h3rogam3r8 <arnavsawant@gmail.com>
2023-11-12 21:23:14 -0500

    Added dynamic web page growth, node value checkers, and added better stylistic decisions. Will be implementing min heaps and max heaps using switch cases in the near future.

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ function clearTree() {
 }
 
 function drawTree() {
-    const canvas = document.getElementById('tree-canvas');
+    const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
